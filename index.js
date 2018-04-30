e347c6195d87cc5653d0f508434dfb340861b9fc
ianfajardo <ianfajardozx@gmail.com>
2018-04-30 14:57:02 -0400

    added additional segements and logic for multi level json

--- index.js
+++ index.js
@@ -2,19 +2,28 @@ var fs = require('fs'),
 		path = require ('path'),
 		filePath = path.join(__dirname, 'ads/internal.html'),
 		minify = require('html-minifier').minify;
+
+function isEmpty(obj) {
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
+    }
+
+    return JSON.stringify(obj) === JSON.stringify({});
+}
 		
 var segments = [
-	//"login_ads",
-	//"login_body",
-	"internal"//,
-	//"agency",
-	//"carrier",
-	//"LEO",
-	//"touroperator",
-	//"3rdparty",
-	//"airport",
-	//"ctd",
-	//"systemprovider"
+	"login_ads",
+	"login",
+	"internal",
+	"agency",
+	"carrier",
+	"LEO",
+	"touroperator",
+	"3rdparty",
+	"airport",
+	"ctd",
+	"systemprovider"
 ];
 
 var text = "";
@@ -26,17 +35,38 @@ var jsonFile = {
 for ( var i = 0; i < segments.length; i++ ) {
 		
 	var segment = segments[i];	
-	console.log(segment);
+	
+	filePath = path.join(__dirname, 'ads/' + segment + '.html');
+	
 	(function(mySegment){
 		fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
 			if(!err) {
-				data = data.split("_")[0];			
+				
+				//split segment name 
+				var segmentVal = mySegment.split("_")[0];
+				var splitVal = mySegment.split("_")[1];
+				
+				//if segment name has additional properties
+				if(splitVal == "" || splitVal == undefined) {
+					splitVal = "body";
+				}
+				
 				var result = data.replace(/\r?\n|\r/g, "").replace(/\s\s+/g, ' ');;
 				result = "" + result.trim() + "";
 				
-				var obj = {"body" : result};
+				var obj = {};
+				
+				obj[splitVal] = result;
+			
+				//if this is not the first thing in the segment, then get additional properties and add to it
+				if(isEmpty(jsonFile[segmentVal]) || jsonFile[segmentVal] != undefined) {
+					var obj1 = jsonFile[segmentVal];
+					var obj2 = obj;
+					for (var attrname in obj1) { obj2[attrname] = obj1[attrname]; }
+					obj = obj2;
+				}
 		
-				jsonFile[mySegment] = obj;
+				jsonFile[segmentVal] = obj;
 				
 				var text = JSON.stringify(jsonFile);
 				
@@ -45,7 +75,7 @@ for ( var i = 0; i < segments.length; i++ ) {
 								return console.log(err);
 						}
 
-						console.log("The file was saved!");
+						//console.log("The file was saved!");
 				}); 
 				
 			}
